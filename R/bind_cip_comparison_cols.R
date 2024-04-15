3f16a7064cc540a44a5501df64bf9633e6bf516c
your name <eli.pousson@gmail.com>
2024-04-15 15:42:43 -0400

    Generalize to allow any reference + comparison cols
    Also filter any records where both reference and comparison columns are missing

--- R/bind_cip_comparison_cols.R
+++ R/bind_cip_comparison_cols.R
@@ -2,26 +2,31 @@
 #'
 #' Used by [summarise_cip_comparison()]
 #'
-bind_cip_comparison_cols <- function(data) {
+bind_cip_comparison_cols <- function(data,
+                                     col_reference = "request",
+                                     col_comparison = "recommendation") {
   data |>
+    filter(
+      !is.na(.data[[col_reference]]) | !is.na(.data[[col_comparison]])
+    ) |>
     mutate(
-      is_new = is.na(request),
-      recommendation = replace_na(recommendation, 0),
-      fy_total_diff = recommendation - request,
-      fy_total_pct_diff = ifelse(
-        recommendation != 0 & request != 0,
-        (recommendation / request) - 1,
+      is_new = is.na(.data[[col_reference]]),
+      "{col_comparison}" := tidyr::replace_na(.data[[col_comparison]], 0),
+      fy_total_diff = .data[[col_comparison]] - .data[[col_reference]],
+      fy_total_pct_diff = if_else(
+        .data[[col_comparison]] != 0 & .data[[col_reference]] != 0,
+        (.data[[col_comparison]] / .data[[col_reference]]) - 1,
         NA_real_
       ),
-      fy_total_pct_diff = ifelse(
-        recommendation == 0 & request > 0,
+      fy_total_pct_diff = if_else(
+        .data[[col_comparison]] == 0 & .data[[col_reference]] > 0,
         -1,
         fy_total_pct_diff
       ),
       diff_desc = case_when(
         is_new ~ "Added funding",
         fy_total_diff > 0 ~ "Increased funding",
-        recommendation == 0 ~ "No funding",
+        .data[[col_comparison]] == 0 ~ "No funding",
         fy_total_diff < 0 ~ "Decreased funding",
         fy_total_diff == 0 ~ "No change"
       )
