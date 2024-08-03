67d8e1330f9a257ad9d36860d90bbb0deed254b2
your name <eli.pousson@gmail.com>
2024-08-02 22:43:36 -0400

    Remove dev scripts + files

--- dev/asset_name_updates.R
+++
@@ -1,3 +0,0 @@
-url <- "https://docs.google.com/spreadsheets/d/1LFjKUq_OgrrvZeXC5rZ9jgqZtqltnG8NLG9NDplvMtg/edit?usp=sharing"
-asset_name_updates <- googlesheets4::read_sheet(url, sheet = "asset_name_updates")
-readr::write_csv(asset_name_updates, path_user_data("asset_name_updates.csv"))
