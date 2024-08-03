bb72d42b165bb384e2af8beb11ed1f8f1b6c701f
your name <eli.pousson@gmail.com>
2024-08-02 23:29:17 -0400

    Remove custom corrections to hierarchy columns
    Swap stopifnot for check_names

--- R/format_p_hierarchy_cols.R
+++ R/format_p_hierarchy_cols.R
@@ -3,61 +3,13 @@ format_p_hierarchy_cols <- function(
     data,
     p_hierarchy_xwalks = NULL,
     ...) {
-  stopifnot(
-    all(has_name(data, c(
+  check_names(
+    data,
+    must.include = c(
       "p_hierarchy1_code", "p_hierarchy2_code",
       "p_hierarchy1_name", "p_hierarchy2_name"
-    )))
-  )
-
-  # FIXME: This is a manual correction that should be
-  # removed when the project is updated in Workday
-  data <- data |>
-    mutate(
-      ## PRJ002543 (added on 2024-01-10)
-      p_hierarchy1_name = if_else(
-        project_code == "PRJ002543",
-        "PJH6100 Public Works",
-        p_hierarchy1_name
-      ),
-      p_hierarchy1_code = if_else(
-        project_code == "PRJ002543",
-        "PJH6100 Public Works",
-        p_hierarchy1_code
-      ),
-      p_hierarchy2_name = if_else(
-        project_code == "PRJ002543",
-        "PJHCIP0525 Capital Projects - Pollution and Erosion Control",
-        p_hierarchy2_name
-      ),
-      p_hierarchy2_code = if_else(
-        project_code == "PRJ002543",
-        "PJHCIP0525 Capital Projects - Pollution and Erosion Control",
-        p_hierarchy2_code
-      ),
-
-      ## PRJ002321 (added on 2024-01-12)
-      p_hierarchy1_name = if_else(
-        project_code == "PRJ002321",
-        "PJH3100 Housing and Community Development",
-        p_hierarchy1_name
-      ),
-      p_hierarchy1_code = if_else(
-        project_code == "PRJ002321",
-        "PJH3100 Housing and Community Development",
-        p_hierarchy1_code
-      ),
-      p_hierarchy2_name = if_else(
-        project_code == "PRJ002321",
-        "PJHCIP0588 Capital Projects - Housing and Community Development",
-        p_hierarchy2_name
-      ),
-      p_hierarchy2_code = if_else(
-        project_code == "PRJ002321",
-        "PJHCIP0588 Capital Projects - Housing and Community Development",
-        p_hierarchy2_code
-      )
     )
+  )
 
   data |>
     mutate(
