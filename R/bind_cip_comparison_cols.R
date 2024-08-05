610b82eaa06e071735b9c285e1189d16ee19d490
your name <eli.pousson@gmail.com>
2024-08-05 09:48:15 -0400

    Rename arguments
    col_reference -> reference_col and col_comparison -> comparison_col

--- R/bind_cip_comparison_cols.R
+++ R/bind_cip_comparison_cols.R
@@ -1,35 +1,35 @@
-#' Bind CIP Comparison columns
+#' Bind columns comparing the reference and comparisong column.
 #'
+#' Adds three comparison columns: fy_total_diff, fy_total_pct_diff, diff_desc.
 #' Used by [summarise_cip_comparison()]
-#'
-bind_cip_comparison_cols <- function(data,
-                                     col_reference = "request",
-                                     col_comparison = "recommendation") {
+bind_cip_comparison_cols <- function(
+    data,
+    reference_col = "request",
+    comparison_col = "recommendation") {
   data |>
     filter(
-      !is.na(.data[[col_reference]]) | !is.na(.data[[col_comparison]]),
-      !(is.na(.data[[col_reference]]) & (.data[[col_comparison]] == 0)),
-      !((.data[[col_reference]] == 0) & (.data[[col_comparison]] == 0))
+      !is.na(.data[[reference_col]]) | !is.na(.data[[comparison_col]]),
+      !(is.na(.data[[reference_col]]) & (.data[[comparison_col]] == 0)),
+      !((.data[[reference_col]] == 0) & (.data[[comparison_col]] == 0))
     ) |>
     mutate(
-      # is_new = is.na(.data[[col_reference]]) | (.data[[col_reference]] == 0),
-      "{col_comparison}" := tidyr::replace_na(.data[[col_comparison]], 0),
-      fy_total_diff = .data[[col_comparison]] - .data[[col_reference]],
+      "{comparison_col}" := tidyr::replace_na(.data[[comparison_col]], 0),
+      fy_total_diff = .data[[comparison_col]] - .data[[reference_col]],
       fy_total_pct_diff = if_else(
-        .data[[col_comparison]] != 0 & .data[[col_reference]] != 0,
-        (.data[[col_comparison]] / .data[[col_reference]]) - 1,
+        .data[[comparison_col]] != 0 & .data[[reference_col]] != 0,
+        (.data[[comparison_col]] / .data[[reference_col]]) - 1,
         NA_real_
       ),
       fy_total_pct_diff = if_else(
-        .data[[col_comparison]] == 0 & .data[[col_reference]] > 0,
+        .data[[comparison_col]] == 0 & .data[[reference_col]] > 0,
         -1,
         fy_total_pct_diff
       ),
       diff_desc = case_when(
-        (is.na(.data[[col_reference]]) | (.data[[col_reference]] == 0)) & .data[[col_comparison]] > 0 ~ "Add funding",
-        (is.na(.data[[col_reference]]) | (.data[[col_reference]] == 0)) & .data[[col_comparison]] < 0 ~ "Transfer funding",
+        (is.na(.data[[reference_col]]) | (.data[[reference_col]] == 0)) & .data[[comparison_col]] > 0 ~ "Add funding",
+        (is.na(.data[[reference_col]]) | (.data[[reference_col]] == 0)) & .data[[comparison_col]] < 0 ~ "Transfer funding",
         fy_total_diff > 0 ~ "Increase funding",
-        .data[[col_reference]] > 0 & .data[[col_comparison]] == 0 ~ "Cut funding",
+        .data[[reference_col]] > 0 & .data[[comparison_col]] == 0 ~ "Cut funding",
         fy_total_diff < 0 ~ "Decrease funding",
         fy_total_diff == 0 ~ "No change"
       )
