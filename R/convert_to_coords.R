154baad76d9fb24e697b23ed06b89df34977d33d
your name <eli.pousson@gmail.com>
2024-08-05 09:49:28 -0400

    Add .f argument to covert_to_coords

--- R/convert_to_coords.R
+++ R/convert_to_coords.R
@@ -1,9 +1,14 @@
 #' Convert a sf object to a data frame of coordinates
 #'
 #' @inheritParams base::round
-convert_to_coords <- function(data, coords = c("lon", "lat"), digits = 8) {
+convert_to_coords <- function(
+    data,
+    coords = c("lon", "lat"),
+    .f = sf::st_centroid,
+    ...,
+    digits = 8) {
   data |>
-    sf::st_centroid() |>
+    .f(...) |>
     suppressWarnings() |>
     sf::st_transform(4326) |>
     sf::st_coordinates() |>
