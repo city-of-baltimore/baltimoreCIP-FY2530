42dcd8745b7f651c3bf5e1ffb9dc6fdb83593b05
your name <eli.pousson@gmail.com>
2024-08-05 21:47:22 -0400

    Refactor join_recommendation_data
    Use report_stage_data instead of bof_recommendation_data for more flexibility

--- R/load_cip_report_data.R
+++ R/load_cip_report_data.R
@@ -1,49 +1,3 @@
-#
-# fy2025_project_locations <- project_requests |>
-#   filter(has_requests_2025 | project_code %in% project_recommendations_update_export$project_code) |>
-#   pull(location_data, name = "project_code") |>
-#   list_rbind(names_to = "project_code") |>
-#   sf::st_as_sf() |>
-#   left_join(
-#     cip_prj_xwalk_update |>
-#       select(
-#         project_code,
-#         cip_number,
-#         cip_project_title = project_title,
-#         cip_number_uncertain = uncertain_match,
-#         first_fiscal_year
-#       ),
-#     relationship = "many-to-many"
-#   ) |>
-#   filter(!sf::st_is_empty(geometry))
-#
-# FY25_dot_locations <- readr::read_rds(here::here("data/FY25_dot_locations.rds")) |>
-#   mutate(
-#     agency_label = "Department of Transportation",
-#     asset_agency_label = "Department of Transportation",
-#     asset_name = case_match(
-#       project_name,
-#       "FY25 - Alley Reconstruction Program" ~ paste0(street_name, " Alley"),
-#       "FY25 - Sidewalk Reconstruction Program" ~ paste0(street_name, " Sidewalk"),
-#       .default = street_name),
-#     asset_address = coalesce(street_address, street_name),
-#     asset_county = "Baltimore city"
-#   ) |>
-#   select(
-#     any_of(names(fy2025_project_locations))
-#   )
-#
-# Locations_FY2025 <- fy2025_project_locations |>
-#   filter(!project_code %in% FY25_dot_locations$project_code) |>
-#   bind_rows(FY25_dot_locations)
-#
-# Locations_FY2025 |>
-#   sfext::write_sf_ext(
-#     "CIP-Locations_FY2025.gpkg",
-#     overwrite = TRUE
-#   )
-
-
 #' Join recommendation data as a nested data frame column to a combined data frame with project details and requests
 #'
 join_recommendation_data <- function(
@@ -91,7 +45,7 @@ combine_cip_project_data <- function(
     project_data,
     request_data,
     pc_recommendation_data = NULL,
-    bof_recommendation_data = NULL,
+    report_stage_data = NULL,
     location_data = NULL,
     current_fy_col = "fy2025",
     summary_col = "total_amt",
@@ -99,7 +53,7 @@ combine_cip_project_data <- function(
     cip_type = "recommendation",
     request_data_col = "request_data",
     pc_recommendation_data_col = "pc_recommendation_data",
-    bof_recommendation_data_col = "recommendation_data",
+    report_stage_data_col = "recommendation_data",
     dictionary) {
   # Rename columns
   project_details <- project_data
@@ -112,7 +66,7 @@ combine_cip_project_data <- function(
   summary_data <- request_data
   if (cip_type == "recommendation") {
     # FIXME: Document this pattern
-    summary_data <- bof_recommendation_data %||% pc_recommendation_data
+    summary_data <- report_stage_data %||% pc_recommendation_data
   }
 
   summary_data_join <- summary_data |>
@@ -156,6 +110,13 @@ combine_cip_project_data <- function(
       .key = request_data_col
     )
 
+  report_stage_data |>
+    prep_to_combine_projects(
+      y = project_data_join_cols,
+      dictionary = dictionary,
+      .key = pc_recommendation_data_col
+    )
+
   # Nest recommendation data
   pc_recommendation_data_join <- pc_recommendation_data |>
     prep_to_combine_projects(
@@ -165,11 +126,11 @@ combine_cip_project_data <- function(
     )
 
   # Nest recommendation data
-  bof_recommendation_data_join <- bof_recommendation_data |>
+  report_stage_data_join <- report_stage_data |>
     prep_to_combine_projects(
       y = project_data_join_cols,
       dictionary = dictionary,
-      .key = bof_recommendation_data_col
+      .key = report_stage_data_col
     )
 
   project_data_join <- project_data |>
@@ -181,23 +142,13 @@ combine_cip_project_data <- function(
       # Add prj reference column
       prj = glue::glue("{project_code}: {project_name}"),
       # Add flags for ease of reporting on nested data
-      has_recommendations = project_code %in% is_recommended_project,
       has_requests = project_code %in% is_requested_project,
-      has_requests_2025 = project_code %in% is_grant_source_request,
-      has_grant_source = project_code %in% is_current_fy_request,
+      has_requests_curr_fy = project_code %in% is_current_fy_request,
+      has_recommendations = project_code %in% is_recommended_project,
+      has_grant_source = project_code %in% is_grant_source_request,
       has_est_cost_data = !(is.na(est_cost_design) & is.na(est_cost_construction) &
         is.na(est_cost_other) & is.na(est_cost_total))
     )
-
-  #   request_data_join_details <- request_data_join |>
-  #     full_join(
-  #       project_details_join |>
-  #         select(project_code) |>
-  #         distinct(),
-  #       by = join_by(project_code),
-  #       relationship = "many-to-one",
-  #       na_matches = "never"
-  #     )
 
   # Combine project details and CIP requests
   project_data_combined <- project_data_join |>
@@ -221,39 +172,14 @@ combine_cip_project_data <- function(
       na_matches = "never"
     ) |>
     left_join(
-      bof_recommendation_data_join,
+      report_stage_data_join,
       by = join_by(project_code),
       relationship = "one-to-one",
       na_matches = "never"
     )
 
-  # if (!is.null(location_data) && rlang::has_name(location_data, "project_code")) {
-  #   if (!rlang::has_name(location_data, "location_data")) {
-  #     location_data <- location_data |>
-  #       filter(!is.na(project_code)) |>
-  #       nest_by(project_code, .key = "location_data", .keep = TRUE)
-  #   }
-  #
-  #   project_data_combined <- project_data_combined |>
-  #     left_join(
-  #       location_data,
-  #       by = join_by(project_code),
-  #       relationship = "one-to-one",
-  #       na_matches = "never"
-  #     )
-  # }
-
   # Save project_requests
   project_data_combined
-}
-
-
-#' Filter a data frame and then pull a distinct vector of project codes
-pull_distinct_project_code <- function(x, ...) {
-  x |>
-    dplyr::filter(...) |>
-    dplyr::distinct(project_code) |>
-    dplyr::pull(project_code)
 }
 
 #' Rename an input data frame based on a dictionary and join to a second data
@@ -275,38 +201,6 @@ prep_to_combine_projects <- function(
     dplyr::nest_by(project_code, .key = .key, .keep = .keep)
 }
 
-
-format_level_cols <- function(data) {
-  data |>
-    mutate(
-      across(
-        ends_with("_level"),
-        \(x) {
-          case_match(
-            x,
-            "Low" ~ "temperature-empty",
-            "Medium" ~ "temperature-half",
-            "High" ~ "temperature-full",
-            .default = NA_character_
-          )
-        },
-        .names = "{.col}_icon"
-      ),
-      across(
-        ends_with("_level"),
-        \(x) {
-          case_match(
-            x,
-            "Low" ~ "#ffae42",
-            "Medium" ~ "#C16512",
-            "High" ~ "#dd4f00",
-            .default = NA_character_
-          )
-        },
-        .names = "{.col}_icon_fill"
-      )
-    )
-}
 
 left_join_asset_data <- function(data,
                                  asset_xwalk,
@@ -363,14 +257,14 @@ load_cip_report_data <- function(
     project_data,
     request_data,
     # FIXME: This has not yet been updated to combine recommendation data
-    pc_recommendation_data,
-    bof_recommendation_data,
+    pc_recommendation_data = NULL,
+    report_stage_data = NULL,
     dictionary) {
   cip_data <- combine_cip_project_data(
     project_data = project_data,
     request_data = request_data,
     pc_recommendation_data = pc_recommendation_data,
-    bof_recommendation_data = bof_recommendation_data,
+    report_stage_data = report_stage_data,
     dictionary = dictionary
   )
 
