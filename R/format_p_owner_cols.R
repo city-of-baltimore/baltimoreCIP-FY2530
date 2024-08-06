866dec5ad2ef76086bcad5e03cac1d6ebad6d99f
your name <eli.pousson@gmail.com>
2024-08-05 21:32:09 -0400

    Fix typo in format owner cols

--- R/format_p_owner_cols.R
+++ R/format_p_owner_cols.R
@@ -1,8 +1,9 @@
+#' Format p_project_owner_name column
 format_p_owner_cols <- function(data) {
   data |>
     format_code_name_cols(
-      code_col = "p_manager_code",
-      name_col = "p_manager_name",
+      code_col = "p_project_owner_code",
+      name_col = "p_project_owner_name",
       pattern = "\\([:alnum:]+\\)",
       .f = str_extract
     ) |>
