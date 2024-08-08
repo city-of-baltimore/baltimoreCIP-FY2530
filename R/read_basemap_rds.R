2c8eb0b3fc8cc1f47e5a05368f777ee5263c6945
your name <eli.pousson@gmail.com>
2024-08-07 22:21:27 -0400

    Fill in docs + use path_tar_user helper

--- R/read_basemap_rds.R
+++ R/read_basemap_rds.R
@@ -1,5 +1,11 @@
+#' Read prepared RDS data with citywide or regional basemap from file
+#'
+#' @param file File name without extension.
+#' See <https://github.com/elipousson/tigris-basemap> for pipeline used to
+#' prepare basemap plot
+#' @importFrom readr read_rds
 read_basemap_rds <- function(file) {
-  path <- path_user_data("basemap", paste0(file, ".rds"))
+  path <- path_tar_user("basemap", paste0(file, ".rds"))
 
   readr::read_rds(path)
 }
